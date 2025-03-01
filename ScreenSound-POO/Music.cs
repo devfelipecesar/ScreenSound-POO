cac00fc7ee074f33b380a09ee6bfcba7153f73a8
User <User@DESKTOP-PLDECF6>
2025-03-01 12:31:37 -0300

    Add project files.

---
+++ ScreenSound-POO/Music.cs
@@ -0,0 +1,32 @@
+﻿class Music // Classe
+{
+    public string nome;
+    public string artista;
+    public int duracao;
+    private bool disponivel;
+
+    public void EscreveDisponivel(bool value)
+    {
+        disponivel = value;
+    }
+
+    public void ExibirFichaTecnica()
+    {
+        Console.WriteLine($"Nome: {nome}");
+        Console.WriteLine($"Artista: {artista}");
+        Console.WriteLine($"Duração: {duracao}");
+        if (disponivel)
+        {
+            Console.WriteLine("Disponível no plano");
+        }
+        else
+        {
+            Console.WriteLine("Adquira o plano Plus!");
+        }
+    }
+
+    public void ExibirMusicaArtista()
+    {
+        Console.WriteLine($"Nome da música / artista: {nome}/{artista}");
+    }
+}
---
+++ ScreenSound-POO/Program.cs
@@ -0,0 +1,21 @@
+﻿Music musica1 = new Music(); //Criação de um objeto
+
+musica1.nome = "Roxane";
+musica1.artista = "The Police";
+musica1.duracao = 273;
+musica1.disponivel = true;
+
+
+Music musica2 = new Music();
+
+musica2.nome = "Vertigo";
+musica2.artista = "U2";
+musica2.duracao = 367;
+musica2.disponivel = false;
+
+
+//musica1.ExibirFichaTecnica();
+//musica2.ExibirFichaTecnica();
+
+musica1.ExibirMusicaArtista();
+musica2.ExibirMusicaArtista();
